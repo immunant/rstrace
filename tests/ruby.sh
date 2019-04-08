1f0f8375674e0194e14cead3da33f00ede848844
Per Larsen <perl@immunant.com>
2019-04-08 01:04:31 -0700

    Work to test on ruby

---
+++ tests/ruby.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+. "$(dirname "$0")/common.sh"
+
+echo "downloading and extracting ruby"
+curl -s https://cache.ruby-lang.org/pub/ruby/2.6/ruby-2.6.2.tar.gz | tar xz
+
+RUBY_HOME="$WORK_DIR/ruby-2.6.2"
+
+# TODO: test all of ruby, not just `ruby` target
+
+cd ${RUBY_HOME}
+./configure --quiet --disable-install-doc
+intercept-build make ruby -j${NUM_PROCS}
+mv compile_commands.json compile_commands.intercept
+make clean
+${CCTRACE} make ruby -j${NUM_PROCS}
+mv compile_commands.json compile_commands.cctrace
+cp compile_commands.* ${SCRIPT_DIR}
+${CCEQ} compile_commands.intercept compile_commands.cctrace
+
+echo -e "${GREEN}PASS${NC}"
